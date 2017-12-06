dd120d0724d55ae90ab5fd3092e0be7f13ba0955
Baldur Blöndal <baldurpet@gmail.com>
2017-12-06 16:05:28 +0000

    Generating Arbitrary values based on a predicate.

---
+++ examples/QC.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE TypeOperators, TypeInType, GADTs, MultiParamTypeClasses,
+             AllowAmbiguousTypes, TypeFamilies, ScopedTypeVariables,
+             RebindableSyntax, ConstraintKinds,
+             FlexibleContexts, LambdaCase, EmptyCase, FlexibleInstances, 
+
+             TemplateHaskell, TypeApplications, UndecidableInstances, 
+             InstanceSigs, DerivingStrategies #-}
+
+module QC where
+
+import Data.Singletons
+import Data.Singletons.TH
+import Data.Singletons.Prelude
+import Prelude
+import Data.Kind
+
+import Test.QuickCheck
+
+newtype ArbBounded a = ArbBounded a deriving (Enum, Bounded) via (a)
+
+instance (Bounded a, Enum a) => Arbitrary (ArbBounded a) where
+  arbitrary :: Gen (ArbBounded a)
+  arbitrary = arbitraryBoundedEnum
+
+singletons [d|
+  data Day = Mon | Tue | Wed | Thu | Fri | Sat | Sun
+    deriving (Eq, Show, Bounded, Enum)
+
+    deriving Arbitrary via (ArbBounded Day)
+
+  isWeekend :: Day -> Bool
+  isWeekend day = day `elem` [Sat, Sun]
+
+  isWeekday :: Day -> Bool
+  isWeekday day = day `elem` [Mon, Tue, Wed, Thu, Fri]
+  |]
+
+instance SingI IsWeekendSym0 where
+  sing :: Sing IsWeekendSym0
+  sing = singFun1 @IsWeekendSym0 sIsWeekend
+
+instance SingI IsWeekdaySym0 where
+  sing :: Sing IsWeekdaySym0
+  sing = singFun1 @IsWeekdaySym0 sIsWeekday
+
+----------------------------------------------------------------------
+-- Adapter that generates an arbitrary values satisfying a predicate
+newtype SuchThat (a::Type) :: (a ~> Bool) -> Type where
+  SuchThat :: a -> SuchThat a predicate
+
+-- From Richard's Effects library.
+type Good a = (Demote a ~ a, SingKind a)
+
+instance (Arbitrary a, Good a, SingI predicate) => Arbitrary (SuchThat a predicate) where
+  arbitrary :: Gen (SuchThat a predicate)
+  arbitrary = SuchThat <$> suchThat arbitrary 
+    (fromSing (sing :: Sing predicate))
+
+-- >>> sample (arbitrary @Weekend)
+-- Sat
+-- Sun
+-- Sun
+-- Sat
+-- Sun
+-- Sat
+-- Sun
+-- Sat
+-- Sun
+-- Sat
+-- Sun
+newtype Weekend = Weekend Day 
+  deriving Show via Day
+  deriving (Arbitrary) via 
+    (SuchThat Day IsWeekendSym0)
+          -- type IsWeekend = FlipSym2 ElemSym0 '[ 'Sat, 'Sun ]
+
+-- >>> sample (arbitrary @Weekday)
+-- Thu
+-- Fri
+-- Thu
+-- Wed
+-- Tue
+-- Mon
+-- Wed
+-- Fri
+-- Wed
+-- Mon
+-- Wed
+newtype Weekday = Weekday Day 
+  deriving Show via Day
+  deriving (Arbitrary) via 
+    (SuchThat Day IsWeekdaySym0)
+          -- type IsWeekday = FlipSym2 ElemSym0 '[ 'Mon, 'Tue, 'Wed, 'Thu, 'Fri ]
+          -- type IsWeekday = NotSym0 :.$$$ IsWeekendIsWeekdaySym0
