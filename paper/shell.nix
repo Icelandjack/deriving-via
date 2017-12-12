12f203ae9c1fd5e7bafc1ed44c345efb37a95233
Andres Loeh <andres@well-typed.com>
2017-12-12 20:07:02 +0100

    Add a shell.nix suitable for building the paper.

---
+++ paper/shell.nix
@@ -0,0 +1,34 @@
+with import <nixpkgs> {};
+
+let
+  ghc-deriving-via = callPackage ../nix/ghc-deriving-via.nix {};
+in
+  stdenv.mkDerivation rec {
+    name = "ghc-deriving-via-env";
+    env = buildEnv {
+      inherit name;
+      paths = buildInputs;
+    };
+    buildInputs = [
+      ghc-deriving-via
+      haskellPackages.lhs2tex
+      (texlive.combine {
+        inherit (texlive)
+          scheme-basic
+          collection-latexextra
+          collection-latexrecommended
+          collection-xetex
+          collection-fontsrecommended
+          kastrup
+          latexmk
+          stmaryrd
+          ec
+          libertine
+          inconsolata
+          newtx
+          boondox
+          mweights
+          zapfding;
+      })
+    ];
+  }
