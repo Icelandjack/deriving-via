831a921f98b099ab33fe92c7521c7cc4e8a983f8
Andres Loeh <andres@well-typed.com>
2018-02-06 20:38:09 +0100

    Update nix expression to include packages.

--- nix/default.nix
+++ nix/default.nix
@@ -1,3 +1,29 @@
 with import <nixpkgs> {};
 
-callPackage ./ghc-deriving-via.nix {}
+let
+
+  ghc-deriving-via = callPackage ./ghc-deriving-via.nix {};
+  ghc-deriving-via-pkgs = with haskell.lib; haskell.packages.ghcHEAD.override {
+    ghc = ghc-deriving-via;
+    overrides = self : super : {
+      mkDerivation = args : super.mkDerivation (args // {
+        doCheck = false;
+        jailbreak = true;
+      });
+      primitive = super.primitive_0_6_3_0;
+      profunctors = super.profunctors_5_2_2;
+      text = super.text_1_2_3_0;
+      unordered-containers = appendPatch super.unordered-containers ./patches/unordered-containers-0.2.8.0.patch;
+    };
+  };
+  ghc-deriving-via-with-pkgs =
+    ghc-deriving-via-pkgs.ghcWithPackages ( pkgs : [
+      pkgs.aeson
+      pkgs.generics-sop
+      pkgs.profunctors
+      pkgs.semigroups
+    ]);
+
+in
+
+  ghc-deriving-via-with-pkgs
